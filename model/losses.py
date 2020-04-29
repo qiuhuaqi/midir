fix mi loss histogram normalisation bug

model/losses.py
@@ -142,16 +142,20 @@ class MILoss(nn.Module):
         self.bin_width_target = (target_max - target_min) / num_bins_target
         self.bin_width_source = (source_max - source_min) / num_bins_source
 
-        bins_target = torch.arange(num_bins_target, requires_grad=False) * self.bin_width_target + target_min
-        bins_source = torch.arange(num_bins_source, requires_grad=False) * self.bin_width_source + source_min
-        self.bins_target = bins_target.float().unsqueeze(1)  # (N, 1, #bins, H*W)
-        self.bins_source = bins_source.float().unsqueeze(1)  # (N, 1, #bins, H*W)
+        bins_target = torch.arange(num_bins_target) * self.bin_width_target + target_min
+        bins_source = torch.arange(num_bins_source) * self.bin_width_source + source_min
 
-        # window width parameter (eps)
+        self.bins_target = bins_target.unsqueeze(1).float()  # (N, 1, #bins, H*W)
+        self.bins_source = bins_source.unsqueeze(1).float()  # (N, 1, #bins, H*W)
+
+        self.bins_target.requires_grad_(False)
+        self.bins_source.requires_grad_(False)
+
+
+        # determine kernel width controlling parameter (eps)
         # to satisfy the partition of unity constraint, eps can be no larger than bin_width
         # and can only be reduced by integer factor to increase kernel support,
         # cubic B-spline function has support of 4*eps, hence maximum number of bins a sample can affect is 4c
-        # todo: what are c(s)? (Look at original Unser paper)
         self.eps_target = c_target * self.bin_width_target
         self.eps_source = c_source * self.bin_width_source
 
@@ -197,12 +201,13 @@ class MILoss(nn.Module):
         W_source = self._parzen_window_1d(D_source, window=self.window)  # (N, #bins, H*W)
 
         # calculate joint histogram (using batch matrix multiplication)
-        histogram_joint = W_target.bmm(W_source.transpose(1, 2))  # (N, #bins, #bins)
-        histogram_joint /= self.eps_target * self.eps_source
+        hist_joint = W_target.bmm(W_source.transpose(1, 2))  # (N, #bins, #bins)
+        hist_joint /= self.eps_target * self.eps_source
 
         """distributions"""
-        # normalise joint histogram to acquire joint distribution
-        p_joint = histogram_joint / histogram_joint.sum()
+        # normalise joint histogram to get joint distribution
+        hist_norm = hist_joint.flatten(start_dim=1, end_dim=-1).sum(dim=1)  # normalisation factor per-image,
+        p_joint = hist_joint / hist_norm.view(-1, 1, 1)  # (N, #bins, #bins) / (N, 1, 1)
 
         # marginalise the joint distribution to get marginal distributions
         # batch size in dim0, target bins in dim1, source bins in dim2
@@ -211,22 +216,22 @@ class MILoss(nn.Module):
 
         """entropy"""
         # calculate entropy
-        entropy_target = - torch.sum(p_target * torch.log(p_target + 1e-12), dim=1)  # (N,1)
-        entropy_source = - torch.sum(p_source * torch.log(p_source + 1e-12), dim=1)  # (N,1)
-        entropy_joint = - torch.sum(p_joint * torch.log(p_joint + 1e-12), dim=(1, 2))  # (N,1)
+        ent_target = - torch.sum(p_target * torch.log(p_target + 1e-12), dim=1)  # (N,1)
+        ent_source = - torch.sum(p_source * torch.log(p_source + 1e-12), dim=1)  # (N,1)
+        ent_joint = - torch.sum(p_joint * torch.log(p_joint + 1e-12), dim=(1, 2))  # (N,1)
 
         """debug mode: store bins, histograms & distributions"""
         if self.debug:
-            self.histogram_joint = histogram_joint
+            self.histogram_joint = hist_joint
             self.p_joint = p_joint
             self.p_target = p_target
             self.p_source = p_source
 
         # return (unnormalised) mutual information or normalised mutual information (NMI)
         if self.nmi:
-            return -torch.mean((entropy_target + entropy_source) / entropy_joint)
+            return -torch.mean((ent_target + ent_source) / ent_joint)
         else:
-            return -torch.mean(entropy_target + entropy_source - entropy_joint)
+            return -torch.mean(ent_target + ent_source - ent_joint)
 
 
 

model/models.py
@@ -2,7 +2,7 @@ import torch
 import torch.nn as nn
 import numpy as np
 
-from model.networks import BaseNet, BaseNetFFD
+from model.networks import SiameseNet, SiameseNetFFD
 from model.transformations import BSplineFFDTransform, DVFTransform
 
 
@@ -21,13 +21,11 @@ class DLRegModel(nn.Module):
         self._set_transform_model()
 
     def _set_network(self):
-        if self.params.network == "BaseNet":
-            print("Using BaseNet")
-            self.network = BaseNet()
+        if self.params.network == "SiameseNet":
+            self.network = SiameseNet()
 
-        elif self.params.network == "BaseNetFFD":
-            print("Using BaseNetFFD")
-            self.network = BaseNetFFD()
+        elif self.params.network == "SiameseNetFFD":
+            self.network = SiameseNetFFD()
 
         else:
             raise ValueError("Network not recognised.")

model/transformations.py
@@ -91,8 +91,8 @@ def spatial_transform(x, dvf, mode="bilinear"):
     h, w = x.size()[-2:]
     grid_h, grid_w = torch.meshgrid([torch.linspace(-1, 1, h), torch.linspace(-1, 1, w)])
 
-    grid_h = grid_h.requires_grad_(requires_grad=False).to(device=x.device)
-    grid_w = grid_w.requires_grad_(requires_grad=False).to(device=x.device)
+    grid_h = grid_h.requires_grad_(False).to(device=x.device)
+    grid_w = grid_w.requires_grad_(False).to(device=x.device)
 
     # (H,W) + (N, H, W) add by broadcasting
     new_grid_h = grid_h + dvf[:, 0, ...]

utils/image.py
@@ -6,9 +6,12 @@ import torch
 
 def normalise_intensity(x,
                         mode="minmax",
-                        min_in=0.0, max_in=255.0,
-                        min_out=0.0, max_out=1.0,
-                        clip=False, clip_range_percentile=(0.05, 99.95),
+                        min_in=0.0,
+                        max_in=255.0,
+                        min_out=0.0,
+                        max_out=1.0,
+                        clip=False,
+                        clip_range_percentile=(0.05, 99.95),
                         ):
     """
     Intensity normalisation (& optional percentile clipping)
@@ -37,7 +40,7 @@ def normalise_intensity(x,
     dim = len(x.shape) - 1
     image_axis = tuple(range(1, 1 + dim))  # (1,2) for 2D; (1,2,3) for 3D
 
-    # Numpy Array version
+    # for numpy.ndarray
     if type(x) is np.ndarray:
 
         # Clipping
@@ -67,7 +70,7 @@ def normalise_intensity(x,
                              "Expect either one of: 'meanstd', 'minmax', 'fixed'")
 
 
-    # Pytorch Tensor version
+    # for torch.Tensor
     elif type(x) is torch.Tensor:
         # todo: clipping not supported at the moment (requires Pytorch version of the np.percentile()
 
@@ -79,6 +82,7 @@ def normalise_intensity(x,
 
         # Normalise minmax
         elif mode is "minmax":
+            # get min/max across dims by flattening first
             min_in = x.flatten(start_dim=1, end_dim=-1).min(dim=1)[0].view(-1, *(1,)*dim)  # (N, (1,)*dim)
             max_in = x.flatten(start_dim=1, end_dim=-1).max(dim=1)[0].view(-1, *(1,)*dim)  # (N, (1,)*dim)
             x = (x - min_in) * (max_out - min_out) / (max_in - min_in + 1e-12)  # (!) multiple broadcasting)