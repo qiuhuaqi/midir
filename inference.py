7b53d9a0412a5f0ef5cde2db59103b204ed24763
Huaqi Qiu <huaqi.qiu@outlook.com>
2020-09-12 11:40:34 +0100

    baseline models

--- inference.py
+++ inference.py
@@ -1,3 +1,4 @@
+"""Run model inference and save outputs for analysis"""
 import os
 import hydra
 from omegaconf import DictConfig
@@ -7,25 +8,45 @@ import numpy as np
 import torch
 
 from data.datasets import BrainLoadingDataset
-from model.baselines import Identity
+from model.baselines import Identity, MirtkFFD, AntsSyN
+from model.lightning import LightningDLReg
 from model.transformations import spatial_transform
 from utils.image_io import save_nifti
+from utils.misc import setup_dir
 
 from analyse import analyse_output
 
-# fix random seeding for eval dataset for fair comparison
 import random
+# seed RNG of eval dataset to get the same pairings for fair comparison
 random.seed(7)
 
 
-def get_model(cfg):
-    model = Identity(cfg.data.dim)
-    # TODO: DL model 1) load model checkpoint; 2) eval mode; 3)put on GPU
+def get_inference_model(cfg):
+    if cfg.model_name == 'dl':
+        # TODO: DL model load model checkpoint; 2) eval mode; 3)put on GPU
+        model = LightningDLReg(hparams=cfg)
+
+    elif cfg.model_name == 'baseline':
+        if cfg.baseline.name == 'Id':
+            model = Identity(cfg.data.dim)
+
+        elif cfg.baseline.name == 'MIRTK':
+            model = MirtkFFD(hparams=cfg.baseline.mirtk_params)
+
+        elif cfg.baseline.name == 'ANTs':
+            model = AntsSyN(hparams=cfg.baseline.ants_params)
+
+        else:
+            raise ValueError(f"Unknown baseline: {cfg.baseline.name}")
+
+    else:
+        raise ValueError(f"Unknown model_name: {cfg.model_name}")
     return model
 
 
 def inference(model, inference_dataset, output_dir, device=torch.device('cpu')):
     print("Running inference:")
+
     for idx, batch in enumerate(tqdm(inference_dataset)):
         # reshape data for inference.yaml
         for k, x in batch.items():
@@ -35,28 +56,21 @@ def inference(model, inference_dataset, output_dir, device=torch.device('cpu')):
                 batch[k] = x.unsqueeze(1).to(device=device)
 
         # model inference
-        batch['dvf_pred'] = model(batch['target'].to(device=device), batch['source'].to(device=device))
+        batch['dvf_pred'] = model(batch['target'], batch['source'])  # TODO: output needs to be as_type input
 
         # deformed images with predicted DVF
-        batch['target_pred'] = spatial_transform(batch['target_original'].to(device=device),
-                                                 batch['dvf_pred'].to(device=device))
-        batch['warped_source'] = spatial_transform(batch['source'].to(device=device),
-                                                   batch['dvf_pred'].to(device=device))
+        batch['target_pred'] = spatial_transform(batch['target_original'], batch['dvf_pred'])
+        batch['warped_source'] = spatial_transform(batch['source'], batch['dvf_pred'])
 
         # deformed segmentation with predicted DVF
-        batch['target_cor_seg_pred'] = spatial_transform(batch['source_cor_seg'].to(device=device),
-                                                         batch['dvf_pred'].to(device=device),
+        batch['target_cor_seg_pred'] = spatial_transform(batch['source_cor_seg'], batch['dvf_pred'],
                                                          interp_mode='nearest')
-        batch['target_subcor_seg_pred'] = spatial_transform(batch['source_subcor_seg'].to(device=device),
-                                                            batch['dvf_pred'].to(device=device),
+        batch['target_subcor_seg_pred'] = spatial_transform(batch['source_subcor_seg'], batch['dvf_pred'],
                                                             interp_mode='nearest')
 
         # save the outputs
-        id = inference_dataset.subject_list[idx]
-        output_id_dir = output_dir + f'/{id}'
-        if not os.path.exists(output_id_dir):
-            os.makedirs(output_id_dir)
-
+        subj_id = inference_dataset.subject_list[idx]
+        output_id_dir = setup_dir(output_dir + f'/{subj_id}')
         for k, x in batch.items():
             x = x.detach().cpu().numpy()
             x = np.moveaxis(x, [0, 1], [-2, -1]).squeeze()
@@ -64,7 +78,7 @@ def inference(model, inference_dataset, output_dir, device=torch.device('cpu')):
             save_nifti(x, path=output_id_dir + f'/{k}.nii.gz')
 
 
-@hydra.main(config_path="conf", config_name="inference")
+@hydra.main(config_path="conf", config_name="config_inference")
 def main(cfg: DictConfig) -> None:
     print(cfg.pretty())
 
@@ -78,10 +92,10 @@ def main(cfg: DictConfig) -> None:
 
     # configure dataset & model
     inference_dataset = BrainLoadingDataset(**cfg.data)
-    model = get_model(cfg)
+    model = get_inference_model(cfg)
 
     # run inference
-    output_dir = os.getcwd() + '/outputs'
+    output_dir = setup_dir(os.getcwd() + '/outputs')
     inference(model, inference_dataset, output_dir, device=device)
 
     # (optional) run analysis on the current inference outputs
--- model/baselines.py
+++ model/baselines.py
@@ -1,18 +1,15 @@
+import os
+import subprocess
+
+import numpy as np
 import torch
+from utils.image_io import save_nifti, load_nifti
 
+MIRTK_PATH = "/vol/medic02/users/hq615/softwares/mirtk-build/mirtk-build-ubuntu18.04/bin/mirtk"
 
-
-class _BaselineModel(object):
+class Identity(object):
     def __init__(self, dim):
         self.dim = dim
-
-    def __call__(self, tar, src):
-        return NotImplementedError
-
-
-class Identity(_BaselineModel):
-    def __init__(self, dim):
-        super(Identity, self).__init__(dim)
 
     def __call__(self, tar, src):
         # identity DVF (N, 2, H, W) or (1, 3, H, W, D)
@@ -21,18 +18,60 @@ class Identity(_BaselineModel):
         return dvf
 
 
-class MirtkFFD(_BaselineModel):
-    def __init__(self, dim):
-        super(MirtkFFD, self).__init__(dim)
+class MirtkFFD(object):
+    """" Baseline method: conventional iterative registration using MIRTK"""
+    def __init__(self, hparams):
+        self.hparams = hparams
+
+        # configure working directory for MIRTK
+        if self.hparams.work_dir is None:
+            self.hparams.work_dir = os.getcwd() + '/workdir'  # TODO: depends on PYTHON CWD, which is hydra.run.dir, bad practice?
+        if not os.path.exists(self.hparams.work_dir):
+            os.makedirs(self.hparams.work_dir)
 
     def __call__(self, tar, src):
-        dvf = None
-        return dvf
+        """Execute MIRTK registration of a volume pair, input/return Tensors"""
+
+        # save the target and source image in work dir
+        tar_path = self.hparams.work_dir + "/target.nii.gz"
+        save_nifti(tar.cpu().numpy()[0, 0, ...], tar_path)
+
+        src_path = self.hparams.work_dir + "/source.nii.gz"
+        save_nifti(src.cpu().numpy()[0, 0, ...], src_path)
+
+        # dof output path
+        dof_path = self.hparams.work_dir + '/dof.dof.gz'
+
+        # register
+        cmd_register = f'{MIRTK_PATH} register {tar_path} {src_path} ' \
+                       f'-sim {self.hparams.sim} ' \
+                       f'-ds {self.hparams.ds} ' \
+                       f'-be {self.hparams.be} ' \
+                       f'-bins {self.hparams.bins} ' \
+                       f'-dofout {dof_path} ' \
+                       f'-model FFD ' \
+                       f'-levels 3 ' \
+                       f'-padding -1 '
+        subprocess.check_call(cmd_register, shell=True)
+
+        # convert dof to dvf
+        dvf_pred_path = self.hparams.work_dir + f'/dvf_pred.nii.gz'
+        cmd_dof_dvf = f'{MIRTK_PATH} convert-dof {dof_path} {dvf_pred_path} ' \
+                      f'-output-format disp_voxel ' \
+                      f'-target {tar_path}'
+        subprocess.check_call(cmd_dof_dvf, shell=True)
+
+        # load converted dvf
+        dvf = load_nifti(dvf_pred_path)[..., 0, :]  # (H, W, D, 3)
+        dvf = np.moveaxis(dvf, -1, 0)[np.newaxis, ...]  # (1, 3, H, W, D)
+
+        return torch.from_numpy(dvf).to(device=tar.device)
 
 
-class AntsSyn(_BaselineModel):
-    def __init__(self, dim):
-        super(AntsSyn, self).__init__(dim)
+
+class AntsSyN(object):
+    def __init__(self, hparams):
+        self.hparams = hparams
 
     def __call__(self, tar, src):
         # dvf = ants_syn_reg(tar, src)
