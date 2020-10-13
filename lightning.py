b6937f1b7889af6a1378ff49081fd86f9d154c18
Huaqi Qiu <huaqi.qiu@outlook.com>
2020-10-13 17:19:27 +0100

    multilevel lightning, best metric logging close #4

--- lightning.py
+++ lightning.py
@@ -10,10 +10,10 @@ from torch.optim import Adam
 # from data.datasets import CamCANSynthDataset, BrainLoadingDataset
 from data.datasets import BrainInterSubject3DTrain, BrainInterSubject3DEval
 
-from model.transformations import spatial_transform
+from model.transformations import spatial_transform, ml_spatial_transform
 from model.utils import get_network, get_transformation, get_loss_fn
 
-from utils.image import bbox_from_mask
+from utils.image import create_img_pyramid
 from utils.metric import measure_metrics
 from utils.visualise import visualise_result
 
@@ -39,7 +39,7 @@ class LightningDLReg(pl.LightningModule):
 
         self.network = get_network(self.hparams)
         self.transformation = get_transformation(self.hparams)
-        self.sim_loss_fn, self.reg_loss_fn = get_loss_fn(self.hparams)
+        self.loss_fn = get_loss_fn(self.hparams)
 
         # initialise best metric
         self.best_metric_result = self.hparams.meta.best_metric_init
@@ -87,48 +87,38 @@ class LightningDLReg(pl.LightningModule):
 
     def forward(self, tar, src):
         net_out = self.network(tar, src)
+        # TODO: multi-resolution transformation (FFD)
         dvf = self.transformation(net_out)
         return dvf
 
-    def _roi_crop(self, x, mask):
-        bbox, _ = bbox_from_mask(mask.squeeze(1).cpu().numpy())
-        # TODO: 1) add Tensor support, 2) move this to utility
-        for i in range(self.hparams.data.dim):
-            x = x.narrow(i + 2, int(bbox[i][0]), int(bbox[i][1] - bbox[i][0]))
-        # returns a view instead of a copy?
-        return x
-
-    def loss_fn(self, batch, warped_source, dvf_pred):
-        target = batch['target']
-
-        # (optional) only evaluate similarity loss within ROI bounding box
-        if self.hparams.loss.loss_roi:
-            assert batch['roi_mask'] is not None, "Loss ROI mask not provided."
-            mask = batch['roi_mask']
-            target = self._roi_crop(target, mask)
-            warped_source = self._roi_crop(warped_source, mask)
-
-        # compute similarity and regularisation losses
-        sim_loss = self.sim_loss_fn(target, warped_source) * self.hparams.loss.sim_weight
-        reg_loss = self.reg_loss_fn(dvf_pred) * self.hparams.loss.reg_weight
-
-        losses = {"loss": sim_loss + reg_loss,
-                  self.hparams.loss.sim_loss: sim_loss,
-                  self.hparams.loss.reg_loss: reg_loss}
-        return losses
-
     def _step(self, batch):
         """ Forward pass inference + compute loss """
-        dvf_pred = self.forward(batch['target'], batch['source'])
-        warped_source = spatial_transform(batch['source'], dvf_pred)
-        losses = self.loss_fn(batch, warped_source, dvf_pred)
 
-        step_dict = {'dvf_pred': dvf_pred,
-                     'warped_source': warped_source}
+        # TODO: +multiresolution
+        dvf_preds = self.forward(batch['target'], batch['source'])
+
+        # create image pyramids
+        tar_pyr = create_img_pyramid(batch['target'], self.hparams.meta.ml_lvls)
+        src_pyr = create_img_pyramid(batch['source'], self.hparams.meta.ml_lvls)
+
+        # warpe source image (pyramid)
+        warped_src_pyr = ml_spatial_transform(src_pyr, dvf_preds)
+
+        # compute loss
+        if self.hparams.loss.loss_roi:
+            roi_mask = batch['roi_mask']
+        else:
+            roi_mask = None
+        losses = self.loss_fn(tar_pyr, warped_src_pyr, dvf_preds, roi_mask=roi_mask)
+
+        step_dict = {'dvf_pred': dvf_preds,
+                     'target': tar_pyr,
+                     'source': src_pyr,
+                     'warped_source': warped_src_pyr}
         return losses, step_dict
 
     def training_step(self, batch, batch_idx):
-        train_losses, step_dict = self._step(batch)
+        train_losses, _ = self._step(batch)
 
         # training logs
         if self.global_step % self.trainer.row_log_interval == 0:
@@ -151,27 +141,41 @@ class LightningDLReg(pl.LightningModule):
         # run inference, compute losses and outputs
         val_losses, step_dict = self._step(batch)
 
+        # collect data for metrics
+        metric_data = dict()
+        for k, x in step_dict.items():
+            # metrics are evaluated using the original resolution
+            metric_data[k] = step_dict[k][-1]
+
         if 'source_seg' in batch.keys():
-            # apply estimated transformation to segmentation
-            step_dict['warped_source_seg'] = spatial_transform(batch['source_seg'], step_dict['dvf_pred'],
-                                                               interp_mode='nearest')
-
+            metric_data['warped_source_seg'] = spatial_transform(batch['source_seg'], step_dict['dvf_pred'][-1],
+                                                                 interp_mode='nearest')
         if 'target_original' in batch.keys():
-            step_dict['target_pred'] = spatial_transform(batch['target_original'], step_dict['dvf_pred'])
+            # compute pyramid for visualisation
+            target_original_pyr = create_img_pyramid(batch['target_original'], lvls=self.hparams.meta.ml_lvls)
+            step_dict['target_original'] = target_original_pyr
+            target_pred_pyr = ml_spatial_transform(target_original_pyr, step_dict['dvf_pred'])
+            step_dict['target_pred'] = target_pred_pyr
+            metric_data['target_pred'] = target_pred_pyr[-1]
 
-        # calculate metric for one validation batch
-        val_data = dict(batch, **step_dict)  # merge data dicts
-        metric_result_step = measure_metrics(val_data,
+        # measure metrics
+        metric_data.update(batch)  # add input data to metric data
+        metric_result_step = measure_metrics(metric_data,
                                              self.hparams.meta.metric_groups,
                                              return_tensor=True)
 
-        # log one figure to visually monitor training
+        # log visualisation figure to Tensorboard
         if batch_idx == 0:
-            val_fig = visualise_result(val_data, axis=2)
-            self.logger.experiment.add_figure('val',
-                                              val_fig,
-                                              global_step=self.global_step,
-                                              close=True)
+            for l in range(self.hparams.meta.ml_lvls):
+                # get data for the current resolution level from step_dict
+                vis_data_l = dict()
+                for k, x in step_dict.items():
+                    vis_data_l[k] = x[l]
+                val_fig_l = visualise_result(vis_data_l, axis=2)
+                self.logger.experiment.add_figure(f'val_lvl{l}',
+                                                  val_fig_l,
+                                                  global_step=self.global_step,
+                                                  close=True)
         return val_losses, metric_result_step
 
     def _log_metrics(self, metric_result):
@@ -193,6 +197,8 @@ class LightningDLReg(pl.LightningModule):
 
     def _check_log_best_metric(self, metric_result):
         """ Update the best metric """
+        # TODO: this should be in model checkpoint the checkpointing callback
+        #  can access the Trainer's logger
         current = metric_result[self.hparams.meta.best_metric]
 
         if self.hparams.meta.best_metric_mode == 'max':
@@ -203,13 +209,14 @@ class LightningDLReg(pl.LightningModule):
             is_best = False
 
         if is_best:
-            # update best metric value
+            # update and log the best metric value
+            self.best_metric_result = current
             self.logger.experiment.add_scalar('best/' + self.hparams.meta.best_metric,
                                               current,
                                               global_step=self.global_step)
 
     def validation_epoch_end(self, outputs):
-        # reduce and log validation loss
+        # average validation loss and log
         losses_list = [x[0] for x in outputs]
         losses_reduced = dict()
         for k in losses_list[0].keys():
@@ -232,4 +239,5 @@ class LightningDLReg(pl.LightningModule):
         self._check_log_best_metric(metric_result_reduced)
 
         # return callback metrics for checkpointing
-        return {'val_loss': losses_reduced['loss'], 'mean_dice_mean': metric_result_reduced['mean_dice_mean']}
+        return {'val_loss': losses_reduced['loss'],
+                'mean_dice_mean': metric_result_reduced['mean_dice_mean']}
