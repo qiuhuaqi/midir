eb54aea8abd99411669b9a9867cb3ce5499a0890
Huaqi Qiu <huaqi.qiu@outlook.com>
2020-12-24 18:01:24 +0800

    multi-res loss refactor

--- core_modules/loss/utils.py
+++ core_modules/loss/utils.py
@@ -1,5 +1,8 @@
+from collections import OrderedDict
+
 import numpy as np
 import torch
+from torch import nn as nn
 from torch.nn import functional as F
 
 
@@ -71,7 +74,6 @@ def finite_diff_oneside(x, direction="forward", boundary="Neumann"):
             x_diff += [x_pad.take(np.arange(1, sizes[i]+1), axis=i+2)
                        - x_pad.take(np.arange(0, sizes[i]), axis=i+2)]
 
-
         elif type(x) is torch.Tensor:
             # Pytorch uses last -> first dimension order
             paddings.reverse()
@@ -96,3 +98,80 @@ def finite_diff_oneside(x, direction="forward", boundary="Neumann"):
             raise TypeError("Input data type not recognised, support numpy.ndarray or torch.Tensor")
 
     return x_diff
+
+
+class MultiResLoss(nn.Module):
+    def __init__(self,
+                 sim_loss_fn,
+                 sim_loss_name,
+                 reg_loss_fn,
+                 reg_loss_name,
+                 reg_weight=0.0,
+                 ml_lvls=1,
+                 ml_weights=None):
+        """
+        Multi-resolution Loss function
+
+        Args:
+            sim_loss_name: (str) Name of the similarity loss
+            reg_loss_name: (str) Name of the regularisation loss
+            reg_weight: (float) Weight on the spatial regularisation term
+            mi_loss_cfg: (dict) MI loss configurations
+            lncc_window_size: (int/tuple/list) Size of the LNCC window
+            ml_lvls: (int) Numbers of levels for multi-resolution
+            ml_weights: (tuple)
+        """
+        super(MultiResLoss, self).__init__()
+
+        self.sim_loss_fn = sim_loss_fn
+        self.sim_loss_name = sim_loss_name
+        self.reg_loss_fn = reg_loss_fn
+        self.reg_loss_name = reg_loss_name
+        self.reg_weight = reg_weight
+
+        # configure multi-resolutions and weighting
+        self.ml_lvls = ml_lvls
+        if ml_weights is None:
+            ml_weights = (1.,) * ml_lvls
+        self.ml_weights = ml_weights
+        assert len(self.ml_weights) == self.ml_lvls
+
+    def forward(self, tars, warped_srcs, flows):
+        assert len(tars) == self.ml_lvls
+        assert len(warped_srcs) == self.ml_lvls
+        assert len(flows) == self.ml_lvls
+
+        # compute loss at multi-level
+        sim_loss = []
+        reg_loss = []
+        loss = []
+
+        losses = OrderedDict()
+        for lvl, (tar, warped_src, flow, weight_l) in enumerate(zip(tars, warped_srcs, flows, self.ml_weights)):
+            sim_loss_l = self.sim_loss_fn(tar, warped_src) * weight_l
+            reg_loss_l = self.reg_loss_fn(flow) * self.reg_weight * weight_l
+            loss_l = sim_loss_l + reg_loss_l
+
+            sim_loss.append(sim_loss_l)
+            reg_loss.append(reg_loss_l)
+            loss.append(loss_l)
+
+            # record weighted losses for all resolution levels
+            if self.ml_lvls > 1:
+                losses.update(
+                    {
+                        f"{self.sim_loss_name}_lv{lvl}": sim_loss_l,
+                        f"{self.reg_loss_name}_lv{lvl}": reg_loss_l,
+                        f"loss_lv{lvl}": loss_l
+                    }
+                )
+
+        # add overall loss to the dict
+        losses.update(
+            {
+                f"{self.sim_loss_name}": torch.sum(torch.stack(sim_loss)),
+                f"{self.reg_loss_name}": torch.sum(torch.stack(reg_loss)),
+                f"loss": torch.sum(torch.stack(loss))
+            }
+        )
+        return losses
